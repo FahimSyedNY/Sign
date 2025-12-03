6ee4405f3184ebc6cbacc6bcdb621cbfd9f83493
FahimSyedNY <fahims13@nycstudents.net>
2025-12-03 11:47:40 -0500

    Fixed semi-colon Added private int msglength

--- src/Sign.java
+++ src/Sign.java
@@ -1,21 +1,23 @@
 public class Sign {
     private String msg;
     private int width;
+    private int msglength;
     public Sign(String msg, int width) {
         this.msg = msg;
         this.width = width;
+        msglength = msg.length();
     }
     public int numberOfLines() {
-        if (msg.length() % width == 0) return msg.length() / width;
-        return msg.length() / width + 1;
+        if (msglength % width == 0) return msglength / width;
+        return msglength / width + 1;
     }
     public String getLines() {
         String result = "";
         String[] list = msg.split("");
         if (msg.isEmpty()) return null;
-        for (int i = 0; i < list.length; i++) {
+        for (int i = 0; i < msglength; i++) {
             result += list[i];
-            if (((i + 1) % width == 0) && i != width) result += ";";
+            if (((i + 1) % width == 0) && i != msglength - 1) result += ";";
         }
         return result;
     }
